913339919990048268b4ac61b664c2b912c20056
Alexandr Borodin <saniborodin2006@gmail.com>
2025-12-17 13:23:23 +0300

    Добавлена форма редактирования

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,7 @@
 const space = document.getElementById("space");
 
-const creatorCheckbox = document.getElementById("creatorCheckbox");
+const creatorRadioBox = document.getElementById("creatorRadioBox");
+const editorRadioBox = document.getElementById("editorRadioBox");
 
 const  getXYonElement = (elem) => {
     const boundingClientRect = elem.getBoundingClientRect();
@@ -14,14 +15,23 @@ const  getXYonElement = (elem) => {
 
 const createDiv = (event) =>{
     const spaceXY = getXYonElement(space);
+
     const newDiv = `<div class="newDiv left-[${event.x- spaceXY.x}px] top-[${event.y - spaceXY.y}px]"></div>`;
+
     space.innerHTML += newDiv;
 }
 
+const editDiv = (event) => {
+    const elem = event.target;
+    console.log(elem.className);
+}
+
 const clickDiv = (event) =>{
-    if (creatorCheckbox.checked){
+    if (creatorRadioBox.checked){
         createDiv(event);
-    } 
+    } else if (editorRadioBox.checked){
+        editDiv(event);
+    }
 }
 
 space.addEventListener("click", (event)=>{clickDiv(event)});
